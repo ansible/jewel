f74e19316137efc336a0c7b5fc64c30a9d5193fc
Jeff Bradberry <jeff.bradberry@gmail.com>
2023-08-14 16:05:30 -0400

    Configure Postgres

--- project/settings.py
+++ project/settings.py
@@ -80,11 +80,14 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["DATABASE_NAME"],
+        "USER": os.environ["DATABASE_USER"],
+        "PASSWORD": os.environ["DATABASE_PASSWORD"],
+        "HOST": os.environ["DATABASE_HOST"],
+        "PORT": os.getenv("DATABASE_PORT", 5432),
     }
 }
 
