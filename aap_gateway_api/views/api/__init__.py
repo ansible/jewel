a66b325b6fc543bd520acaeb09cfd0900c9efa09
John Westcott IV <john.westcott.iv@redhat.com>
2023-08-17 08:14:01 -0400

    Fixing login/logout pages

--- aap_gateway_api/views/api/__init__.py
+++ aap_gateway_api/views/api/__init__.py
@@ -1,4 +1,5 @@
 from aap_gateway_api.views.api.environment import EnvironmentViewSet  # noqa: 401
+from aap_gateway_api.views.api.local_login import LoggedLoginView, LoggedLogoutView  # noqa: 401
 from aap_gateway_api.views.api.me import MeViewSet  # noqa: 401
 from aap_gateway_api.views.api.organization import OrganizationViewSet  # noqa: 401
 from aap_gateway_api.views.api.service import ServiceViewSet  # noqa: 401
