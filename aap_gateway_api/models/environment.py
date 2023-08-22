589e6cb305d8eb4633d7b603e7057b12e18e6f86
John Westcott IV <john.westcott.iv@redhat.com>
2023-08-22 15:16:54 -0400

    Refactoring related and summary fields

--- aap_gateway_api/models/environment.py
+++ aap_gateway_api/models/environment.py
@@ -1,5 +1,3 @@
-from rest_framework.reverse import reverse_lazy
-
 from aap_gateway_api.models.common import NamedCommonModel
 
 
@@ -8,13 +6,4 @@ class Environment(NamedCommonModel):
         app_label = 'aap_gateway_api'
         ordering = ('name',)
 
-    # Return NamedCommonModel summary and related fields
-    def get_summary_fields(self):
-        return {}
-
-    # What items are related to this object
-    def related_fields(self, request):
-        res = super().related_fields(request)
-        res['organizations'] = reverse_lazy('environment-organizations', kwargs={'pk': self.pk}, request=request)
-        res['services'] = reverse_lazy('environment-services', kwargs={'pk': self.pk}, request=request)
-        return res
+    reverse_foreign_key_fields = ['organizations', 'services']
--- aap_gateway_api/models/service.py
+++ aap_gateway_api/models/service.py
@@ -1,7 +1,6 @@
 from urllib.parse import urljoin
 
 from django.db import models
-from rest_framework.reverse import reverse_lazy
 
 from aap_gateway_api.models.common import NamedCommonModel
 from aap_gateway_api.models.environment import Environment
@@ -26,23 +25,8 @@ class Service(NamedCommonModel):
     def get_jwt_login_url(self):
         return urljoin(self.url_to_proxy_to, 'api/gateway/login')
 
-    def get_summary_fields(self):
-        res = {}
-        if self.environment:
-            res['environment'] = self.environment.summary_fields()
-        if self.created_by:
-            res['created_by'] = self.created_by.summary_fields()
-        if self.modified_by:
-            res['modified_by'] = self.modified_by.summary_fields()
-        return res
-
+    # Add the service_type to the summary fields
     def summary_fields(self):
         res = super.summary_fields()
-        res['type'] = self.service_type
-        return res
-
-    # What items are related to this object
-    def related_fields(self, request):
-        res = super().related_fields(request)
-        res['environment'] = reverse_lazy('environment-detail', kwargs={'pk': self.environment.pk}, request=request)
+        res['service_type'] = self.service_type
         return res
