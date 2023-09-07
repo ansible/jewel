895541f720bcf42905ea4a3264608d1d4f8de5f3
John Westcott IV <john.westcott.iv@redhat.com>
2023-09-07 11:58:29 -0400

    Remove signals for ldap authenticator

--- aap_gateway_api/authentication/backends.py
+++ aap_gateway_api/authentication/backends.py
@@ -3,6 +3,9 @@ from collections import OrderedDict
 
 from django.contrib.auth.backends import ModelBackend
 
+from aap_gateway_api.authentication.ldap.backends import BaseLDAPBackend, LDAPSettings
+from aap_gateway_api.models import Authenticator
+
 logger = logging.getLogger('aap.gateway.authentication.backend')
 
 authentication_backends = OrderedDict()
@@ -10,15 +13,26 @@ authentication_backends = OrderedDict()
 
 class GatewayAuth(ModelBackend):
     def authenticate(self, request, username=None, password=None):
-        for backend in authentication_backends.values():
-            user = backend.authenticate(request, username=username, password=password)
+        for backend in Authenticator.objects.filter(type='l', enabled=True):
+            self.create_or_update_ldap_adapter(backend)
+            user = authentication_backends[backend.id].authenticate(request, username=username, password=password)
             if user:
                 return user
 
         return None
 
+    def create_or_update_ldap_adapter(self, authenticator):
+        needs_change = False
+        if authenticator.id not in authentication_backends:
+            logger.debug(f"Creating LDAP adapter for {authenticator.name}")
+            authentication_backends[authenticator.id] = BaseLDAPBackend(authenticator=authenticator)
+            needs_change = True
 
-def build_all_authenticators():
-    from aap_gateway_api.authentication.ldap.ldap_backends import build_ldap_authenticators
+        if needs_change or authenticator.modified_on != authentication_backends[authenticator.id].authenticator.modified_on:
+            if not needs_change:
+                logger.debug(f"Updating LDAP adapter {authenticator.name}")
 
-    build_ldap_authenticators()
+            authentication_backends[authenticator.id].settings = LDAPSettings(defaults=authenticator.configuration)
+            authentication_backends[authenticator.id].authenticator = authenticator
+        else:
+            logger.debug(f"No updated needed for LDAP adapter {authenticator.name}")
