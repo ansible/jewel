b2af4c9a0e00fef355b6667a7b42d429c58cae22
Rick Elrod <rick@elrod.me>
2023-09-08 15:46:58 -0500

    Some coverage for edge cases in preferences
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- aap_gateway_api/tests/unit/test_preferences.py
+++ aap_gateway_api/tests/unit/test_preferences.py
@@ -1,6 +1,7 @@
 import pytest
 
 from aap_gateway_api.utils import preferences
+from aap_gateway_api.utils import ENCRYPTED_STRING
 
 
 def test_register_exceptions():
@@ -10,4 +11,32 @@ def test_register_exceptions():
 
     with pytest.raises(NotImplementedError) as e:
         preferences.register(preference_name="foo", preference_type="some_weird_type")
-    assert str(e.value) == "Preference type some_weird_type is not yet implemented in preferences utils"
+    assert (
+        str(e.value)
+        == "Preference type some_weird_type is not yet implemented in preferences utils"
+    )
+
+
+def test_get_preference_value_exceptions():
+    with pytest.raises(ValueError) as e:
+        preferences.get_preference_value("", "foo")
+    assert str(e.value) == "You must pass get_preference_value a section and a name"
+
+    with pytest.raises(ValueError) as e:
+        preferences.get_preference_value("foo", "")
+    assert str(e.value) == "You must pass get_preference_value a section and a name"
+
+
+def test_encrypted_preference():
+    preferences.register(
+        section="general",
+        preference_name="enc_test",
+        encrypted=True,
+        preference_type="string",
+    )
+    assert preferences.get_preference_value("general", "enc_test") == ENCRYPTED_STRING
+
+
+def test_get_preference_sections():
+    sections = preferences.get_preference_sections()
+    assert "general" in sections
