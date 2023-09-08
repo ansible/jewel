39aabb4feb3a1e79ad3aab454ee08ccebc189f23
Rick Elrod <rick@elrod.me>
2023-09-08 13:59:14 -0500

    Define two test database names to please Django
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- aap_gateway_api/tests/settings_overrides.py
+++ aap_gateway_api/tests/settings_overrides.py
@@ -3,8 +3,9 @@ from aap_gateway_api.settings import *
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "tests/db.sqlite3",
         "TEST": {
-            "NAME": "tests/testdb.sqlite3",
+            "NAME": "tests/db_test.sqlite3",
         },
     }
 }
