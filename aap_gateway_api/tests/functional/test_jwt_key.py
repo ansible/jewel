f2ed77afd77661a93d62ef3389335ba7f784d6ad
Rick Elrod <rick@elrod.me>
2023-09-09 17:33:55 -0500

    Add test for jwt_key exceptional cases
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- aap_gateway_api/tests/functional/test_jwt_key.py
+++ aap_gateway_api/tests/functional/test_jwt_key.py
@@ -1,4 +1,6 @@
 import pytest
+from unittest import mock
+from unittest.mock import MagicMock
 
 from django.urls import reverse
 
@@ -11,3 +13,35 @@ def test_jwt_key(unauthenticated_api_client):
     body = response.content.decode("utf-8")
     assert body.startswith("-----BEGIN PUBLIC KEY-----")
     assert body.endswith("-----END PUBLIC KEY-----\n")
+
+
+@mock.patch("aap_gateway_api.views.api.v1.jwt_key.logger")
+@mock.patch("aap_gateway_api.views.api.v1.jwt_key.get_jwt_rsa_key", return_value="")
+def test_jwt_key_bad_key(
+    get_jwt_rsa_key, logger, unauthenticated_api_client, shut_up_logging
+):
+    unauthenticated_api_client.raise_request_exception = False
+    url = reverse("jwt-key-view")
+    response = unauthenticated_api_client.get(url)
+    assert response.status_code == 500
+    assert logger.exception.call_count == 1
+    assert logger.exception.call_args[0][0] == "Unable to load private key from JWT key"
+
+
+@mock.patch("aap_gateway_api.views.api.v1.jwt_key.logger")
+def test_jwt_key_bad_public_key(logger, unauthenticated_api_client, shut_up_logging):
+    mock_private_key = MagicMock()
+    mock_private_key.public_key().public_bytes.side_effect = Exception("Test Exception")
+    with mock.patch(
+        "aap_gateway_api.views.api.v1.jwt_key.serialization.load_pem_private_key",
+        return_value=mock_private_key,
+    ):
+        unauthenticated_api_client.raise_request_exception = False
+        url = reverse("jwt-key-view")
+        response = unauthenticated_api_client.get(url)
+        assert response.status_code == 500
+        assert logger.exception.call_count == 1
+        assert (
+            logger.exception.call_args[0][0]
+            == "Unable to export public key from JWT key"
+        )
