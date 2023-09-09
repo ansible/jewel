415fe02564aae6547cac2a6c9c0aff365f430ecc
Red Hat Contributor <noreply@redhat.com>
2023-09-08 23:51:33 -0500

    Add coverage for LoggedBasicAuthentication
    Signed-off-by: Red Hat Contributor <noreply@redhat.com>

---
+++ aap_gateway_api/tests/functional/authentication/test_logged_basic_auth.py
@@ -0,0 +1,75 @@
+import base64
+import pytest
+from unittest import mock
+
+from django.urls import reverse
+
+from aap_gateway_api.authentication.basic_auth import LoggedBasicAuthentication
+
+
+# Patch the logger to ensure we log the correct message
+@mock.patch("aap_gateway_api.authentication.basic_auth.logged_basic_auth.logger")
+def test_logged_basic_auth(
+    logger, unauthenticated_api_client, organization, admin_user
+):
+    client = unauthenticated_api_client
+    client.credentials(
+        HTTP_AUTHORIZATION="Basic "
+        + base64.b64encode("admin:password".encode("utf-8")).decode("utf-8")
+    )
+    url = reverse("organization-list")
+    response = client.get(url)
+    assert response.status_code == 200
+    assert logger.info.call_count == 1
+    expected = f"User admin performed a GET to {url} through the API via basic auth"
+    assert logger.info.call_args[0][0] == expected
+
+
+def test_logged_basic_auth_disabled(
+    unauthenticated_api_client, organization, admin_user, settings, set_preference
+):
+    set_preference("proxy", "gateway_basic_auth_enabled", False)
+
+    client = unauthenticated_api_client
+    client.credentials(
+        HTTP_AUTHORIZATION="Basic "
+        + base64.b64encode("admin:password".encode("utf-8")).decode("utf-8")
+    )
+    url = reverse("organization-list")
+    response = client.get(url)
+    assert response.status_code == 403
+
+
+# There is really no better way to do this. I tried. Really hard.
+# We can't use the 'settings' fixture here. Because even though DRF gets notified of the change,
+# the view already has the old settings as local variables. So we have to patch the view to fix that.
+@mock.patch(
+    "rest_framework.views.APIView.authentication_classes", [LoggedBasicAuthentication]
+)
+def test_logged_basic_auth_invalid(unauthenticated_api_client, organization):
+    client = unauthenticated_api_client
+    client.credentials(
+        HTTP_AUTHORIZATION="Basic "
+        + base64.b64encode("admin:wrongPassw0rd".encode("utf-8")).decode("utf-8")
+    )
+    url = reverse("organization-list")
+    response = client.get(url)
+    assert response.status_code == 401
+
+
+@mock.patch(
+    "rest_framework.views.APIView.authentication_classes", [LoggedBasicAuthentication]
+)
+def test_logged_basic_auth_invalid_disabled(
+    unauthenticated_api_client, organization, set_preference
+):
+    set_preference("proxy", "gateway_basic_auth_enabled", False)
+    client = unauthenticated_api_client
+    client.credentials(
+        HTTP_AUTHORIZATION="Basic "
+        + base64.b64encode("admin:wrongPassw0rd".encode("utf-8")).decode("utf-8")
+    )
+    url = reverse("organization-list")
+    response = client.get(url)
+    # TODO: Why do we return 403 here from authenticate_header? Is this intentional?
+    assert response.status_code == 403
