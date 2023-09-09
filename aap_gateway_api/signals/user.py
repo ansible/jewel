12ccea32e3f73259ea3c29835fa6b287fc2e7588
Red Hat Contributor <noreply@redhat.com>
2023-09-09 03:33:08 -0500

    Hook in the user logout signal and test it
    Signed-off-by: Red Hat Contributor <noreply@redhat.com>

--- aap_gateway_api/signals/user.py
+++ aap_gateway_api/signals/user.py
@@ -8,6 +8,6 @@ logger = logging.getLogger('aap.gateway.signals.user')
 
 @receiver(user_logged_out)
 def user_logged_out(sender, user, request, **kwargs):
-    logger.info("user logged out: %s at %s" % (user, request.META['REMOTE_ADDR']))
+    logger.info(f"User logged out: {user} at {request.META.get('REMOTE_ADDR', '<no remote address>')}")
     # Remove the sessions so they will be repopulated on login
     user.logout()
