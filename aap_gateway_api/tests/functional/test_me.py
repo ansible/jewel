9a55a236b0c3e27c5bb4eb23ff555ea110799570
Red Hat Contributor <noreply@redhat.com>
2023-09-09 16:59:05 -0500

    Add tests for /me
    Signed-off-by: Red Hat Contributor <noreply@redhat.com>

---
+++ aap_gateway_api/tests/functional/test_me.py
@@ -0,0 +1,16 @@
+import pytest
+
+from django.urls import reverse
+
+
+def test_me(admin_api_client):
+    url = reverse("me-list")
+    response = admin_api_client.get(url)
+    assert response.status_code == 200
+    assert response.data["results"][0]["username"] == "admin"
+
+
+def test_me_unauthenticated(unauthenticated_api_client):
+    url = reverse("me-list")
+    response = unauthenticated_api_client.get(url)
+    assert response.status_code == 403
