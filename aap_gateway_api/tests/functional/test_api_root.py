abbf8f558b91cc85cc0ba60750dae85a6d98f160
Red Hat Contributor <noreply@redhat.com>
2023-09-10 01:46:14 -0500

    Add test for root API view
    Signed-off-by: Red Hat Contributor <noreply@redhat.com>

---
+++ aap_gateway_api/tests/functional/test_api_root.py
@@ -0,0 +1,16 @@
+from django.urls import reverse
+
+
+def test_api_root_view(unauthenticated_api_client):
+    """
+    Test the API root view.
+    """
+    url = reverse("api_root_view")
+    response = unauthenticated_api_client.get(url)
+    assert response.status_code == 200
+    assert "description" in response.data
+    assert response.data["description"] == "Gateway REST API"
+
+    gateway = reverse("api_gateway_root_view")
+    assert "gateway" in response.data
+    assert response.data["gateway"]["gateway"] == gateway
