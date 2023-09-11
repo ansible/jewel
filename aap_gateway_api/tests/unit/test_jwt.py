c7b240f1a1ea11ce2096015591c3355ebd966f02
Rick Elrod <rick@elrod.me>
2023-09-11 01:38:11 -0500

    Add a JWT decode function to utils.jwt_token
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- aap_gateway_api/tests/unit/test_jwt.py
+++ aap_gateway_api/tests/unit/test_jwt.py
@@ -1,17 +1,12 @@
 import jwt
 import pytest
 
-from aap_gateway_api.utils.jwt_token import create_signed_jwt, get_jwt_rsa_key
+from aap_gateway_api.utils.jwt_token import create_signed_jwt, decode_signed_jwt
 
 
 def test_jwt_token_encode_decode(admin_user):
     jwt_token = create_signed_jwt(admin_user)
-    decoded = jwt.decode(
-        jwt_token,
-        get_jwt_rsa_key(public=True),
-        algorithms=["RS256"],
-        audience="aap-services",
-    )
+    decoded = decode_signed_jwt(jwt_token)
     assert decoded["sub"] == admin_user.username
     assert decoded["email"] == admin_user.email
     assert decoded["iss"] == "aap-gateway"
