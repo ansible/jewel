14255f3ee7da2dacaa8883c95a242da70c13e125
John Westcott IV <john.westcott.iv@redhat.com>
2023-09-15 08:26:37 -0400

    Fix issue where logout signal would stack if no user was logged in

--- aap_gateway_api/signals/user.py
+++ aap_gateway_api/signals/user.py
@@ -8,6 +8,7 @@ logger = logging.getLogger('aap.gateway.signals.user')
 
 @receiver(user_logged_out)
 def user_logged_out(sender, user, request, **kwargs):
-    logger.info(f"User logged out: {user} at {request.META.get('REMOTE_ADDR', '<no remote address>')}")
-    # Remove the sessions so they will be repopulated on login
-    user.logout()
+    if user:
+        logger.info(f"User logged out: {user} at {request.META.get('REMOTE_ADDR', '<no remote address>')}")
+        # Remove the sessions so they will be repopulated on login
+        user.logout()
