d136237f5dd892491617e8b1a0d8a50f5a68ea2b
Rick Elrod <rick@elrod.me>
2023-09-15 12:56:23 -0500

    Add tests for updating prefs with wrong type value
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- aap_gateway_api/tests/functional/test_settings.py
+++ aap_gateway_api/tests/functional/test_settings.py
@@ -1,3 +1,4 @@
+import pytest
 from django.urls import reverse
 
 from aap_gateway_api.models import Preference
@@ -55,3 +56,37 @@ def test_set_setting_invalid(admin_api_client):
     response = admin_api_client.put(url, data={"nonexistent_setting": "X-FOO-BAR"})
     assert response.status_code == 400
     assert Preference.objects.filter(name="nonexistent_setting").count() == 0
+
+
+@pytest.mark.parametrize(
+    "preference_type, default, value, err_substring",
+    [
+        # NOTE: If you add cases to this matrix, add them to in unit/test_preferences.py, too.
+        ("string", "foo", 1234, None),  # Apparently this gets coerced to a string
+        ("string", "foo", True, None),  # Apparently this gets coerced to a string
+        ("bool", False, "true", "true is not a boolean"),
+        ("bool", False, 1, "1 is not a boolean"),
+        ("int", 0, "not an int", "IntSerializer can only serialize int values"),
+        ("int", 0, False, "IntSerializer can only serialize int values"),
+        ("url", "https://example.com", 1337, "1337 is not a valid URL"),
+    ],
+)
+def test_set_setting_bad_type(admin_api_client, register_preference, preference_type, default, value, err_substring):
+    """
+    Test setting a preference via the API with a value of the wrong type.
+    """
+    register_preference(
+        section="general",
+        preference_name="bad_type",
+        default=default,
+        encrypted=False,
+        preference_type=preference_type,
+    )
+
+    url = reverse("setting-section-list", kwargs={"category_slug": "general"})
+    response = admin_api_client.put(url, data={"bad_type": value})
+    assert response.status_code == 200 if err_substring is None else 400
+
+    if err_substring is not None:
+        assert response.data["bad_type"].code == "invalid"
+        assert str(response.data["bad_type"]) == err_substring
