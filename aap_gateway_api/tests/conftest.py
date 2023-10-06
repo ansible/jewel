3f0548a9489954555f66fd32f4d6c33a48600aa8
Red Hat Contributor <noreply@redhat.com>
2023-10-06 11:05:35 -0400

    Remove and split symlinked conftest.py
    Signed-off-by: Red Hat Contributor <noreply@redhat.com>

--- aap_gateway_api/tests/conftest.py
+++ aap_gateway_api/tests/conftest.py
@@ -4,6 +4,7 @@ import uuid
 import pytest
 
 from aap_gateway_api.models import AdditionalRoute, ServiceAPIRoute, ServiceCluster, ServiceNode
+from ansible_base.tests.conftest import admin_api_client, shut_up_logging, unauthenticated_api_client, user  # noqa: F401
 
 
 def copy_fixture(copies=1):
@@ -23,32 +24,6 @@ def copy_fixture(copies=1):
         return func
 
     return wrapper
-
-
-@pytest.fixture
-def unauthenticated_api_client(db):
-    from rest_framework.test import APIClient
-
-    return APIClient()
-
-
-@pytest.fixture
-def admin_api_client(db, admin_user, unauthenticated_api_client):
-    client = unauthenticated_api_client
-    client.login(username="admin", password="password")
-    yield client
-    try:
-        client.logout()
-    except AttributeError:
-        # The test might have logged the user out already (e.g. to test the logout signal)
-        pass
-
-
-@pytest.fixture
-def user(db, django_user_model):
-    user = django_user_model.objects.create_user(username="user", password="password")
-    yield user
-    user.delete()
 
 
 @pytest.fixture
@@ -76,18 +51,6 @@ def set_preference(db):
     yield _set_preference
     for pref in old_prefs:
         update_preference_value(pref.section, pref.name, pref.value)
-
-
-@pytest.fixture
-def shut_up_logging():
-    """
-    This fixture allows you to temporarily disable logging for a test.
-    """
-    import logging
-
-    logging.disable(logging.CRITICAL)
-    yield
-    logging.disable(logging.NOTSET)
 
 
 @pytest.fixture
@@ -139,34 +102,6 @@ def rsa_keypair():
     from aap_gateway_api.utils.jwt_token import generate_jwt_keypair
 
     return generate_jwt_keypair()
-
-
-@pytest.fixture
-def ldap_configuration():
-    return {
-        "SERVER_URI": ["ldap://ldap06.example.com:389"],
-        "BIND_DN": "cn=ldapadmin,dc=example,dc=org",
-        "BIND_PASSWORD": "securepassword",
-        "START_TLS": False,
-        "CONNECTION_OPTIONS": {"OPT_REFERRALS": 0, "OPT_NETWORK_TIMEOUT": 30},
-        "USER_SEARCH": ["ou=users,dc=example,dc=org", "SCOPE_SUBTREE", "(cn=%(user)s)"],
-        "USER_DN_TEMPLATE": "cn=%(user)s,ou=users,dc=example,dc=org",
-        "USER_ATTR_MAP": {"email": "mail", "last_name": "sn", "first_name": "givenName"},
-        "GROUP_SEARCH": ["ou=groups,dc=example,dc=org", "SCOPE_SUBTREE", "(objectClass=groupOfNames)"],
-        "GROUP_TYPE": "MemberDNGroupType",
-        "GROUP_TYPE_PARAMS": {"name_attr": "cn", "member_attr": "member"},
-    }
-
-
-@pytest.fixture
-def ldap_authenticator(ldap_configuration):
-    from ansible_base.models import Authenticator
-
-    authenticator = Authenticator.objects.create(
-        name="Test LDAP Authenticator", enabled=True, create_objects=True, users_unique=False, remove_users=True, type="l", configuration=ldap_configuration
-    )
-    yield authenticator
-    authenticator.delete()
 
 
 @pytest.fixture
--- ansible_base/tests/conftest.py
+++ ansible_base/tests/conftest.py
@@ -1,1 +1,67 @@
-../../aap_gateway_api/tests/conftest.py
+import pytest
+
+
+@pytest.fixture
+def unauthenticated_api_client(db):
+    from rest_framework.test import APIClient
+
+    return APIClient()
+
+
+@pytest.fixture
+def admin_api_client(db, admin_user, unauthenticated_api_client):
+    client = unauthenticated_api_client
+    client.login(username="admin", password="password")
+    yield client
+    try:
+        client.logout()
+    except AttributeError:
+        # The test might have logged the user out already (e.g. to test the logout signal)
+        pass
+
+
+@pytest.fixture
+def user(db, django_user_model):
+    user = django_user_model.objects.create_user(username="user", password="password")
+    yield user
+    user.delete()
+
+
+@pytest.fixture
+def shut_up_logging():
+    """
+    This fixture allows you to temporarily disable logging for a test.
+    """
+    import logging
+
+    logging.disable(logging.CRITICAL)
+    yield
+    logging.disable(logging.NOTSET)
+
+
+@pytest.fixture
+def ldap_configuration():
+    return {
+        "SERVER_URI": ["ldap://ldap06.example.com:389"],
+        "BIND_DN": "cn=ldapadmin,dc=example,dc=org",
+        "BIND_PASSWORD": "securepassword",
+        "START_TLS": False,
+        "CONNECTION_OPTIONS": {"OPT_REFERRALS": 0, "OPT_NETWORK_TIMEOUT": 30},
+        "USER_SEARCH": ["ou=users,dc=example,dc=org", "SCOPE_SUBTREE", "(cn=%(user)s)"],
+        "USER_DN_TEMPLATE": "cn=%(user)s,ou=users,dc=example,dc=org",
+        "USER_ATTR_MAP": {"email": "mail", "last_name": "sn", "first_name": "givenName"},
+        "GROUP_SEARCH": ["ou=groups,dc=example,dc=org", "SCOPE_SUBTREE", "(objectClass=groupOfNames)"],
+        "GROUP_TYPE": "MemberDNGroupType",
+        "GROUP_TYPE_PARAMS": {"name_attr": "cn", "member_attr": "member"},
+    }
+
+
+@pytest.fixture
+def ldap_authenticator(ldap_configuration):
+    from ansible_base.models import Authenticator
+
+    authenticator = Authenticator.objects.create(
+        name="Test LDAP Authenticator", enabled=True, create_objects=True, users_unique=False, remove_users=True, type="l", configuration=ldap_configuration
+    )
+    yield authenticator
+    authenticator.delete()
