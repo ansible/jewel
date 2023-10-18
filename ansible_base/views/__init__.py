870c81b5764a6f88c119b9279dd0f599bc540a2a
John Westcott IV <john.westcott.iv@redhat.com>
2023-10-17 21:03:40 -0400

    Add view for UI to know which options to show on login screen

--- ansible_base/views/__init__.py
+++ ansible_base/views/__init__.py
@@ -2,3 +2,4 @@ from .authenticator import AuthenticatorAuthenticatorMapViewSet, AuthenticatorVi
 from .authenticator_map import AuthenticatorMapViewSet  # noqa: F401
 from .authenticator_plugins import AuthenticatorPluginView  # noqa: F401
 from .trigger_definition import TriggerDefinitionView  # noqa: F401
+from .ui_auth import UIAuth  # noqa: F401
