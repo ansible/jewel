b7771e49b21bd0be6db2508d03c8ad4ae651ad88
David Newswanger <dnewswan@redhat.com>
2023-10-20 15:03:01 -0600

    add filtering and sorting

--- ansible_base/views/authenticator.py
+++ ansible_base/views/authenticator.py
@@ -1,6 +1,7 @@
 from rest_framework import permissions
 from rest_framework.viewsets import ModelViewSet
 
+from aap_gateway_api.views.api.v1.common import CustomFilterBackend
 from ansible_base.models import Authenticator, AuthenticatorMap
 from ansible_base.serializers import AuthenticatorMapSerializer, AuthenticatorSerializer
 
@@ -10,6 +11,7 @@ class AuthenticatorViewSet(ModelViewSet):
     API endpoint that allows groups to be viewed or edited.
     """
 
+    filter_backends = (CustomFilterBackend,)
     queryset = Authenticator.objects.all()
     serializer_class = AuthenticatorSerializer
     permission_classes = [permissions.IsAuthenticated]
