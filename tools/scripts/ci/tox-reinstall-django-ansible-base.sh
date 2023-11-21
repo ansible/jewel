4fcdb5f3dbe1023cd48fac45e467e770d43c0430
Rick Elrod <rick@elrod.me>
2023-11-21 12:29:20 -0700

    [devexp] Only reinstall DAB in tox env when needed (#141)
    Instead of reinstalling django-ansible-base every single time tox runs,
    cache the requirements files inside the tox venv dir, and only trigger a
    reinstall if they have changed.
    
    Add django-ansible-base to the PYTHONPATH and test against it directly
    on disk rather than copying it into the venv.
    
    On average, locally this shaves around 3 seconds off of each tox
    environment that depends on django-ansible-base every time tests are
    run.
    
    Signed-off-by: Rick Elrod <rick@elrod.me>

---
+++ tools/scripts/ci/tox-reinstall-django-ansible-base.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env sh
+#
+# NOTE: This script is only meant to be used by tox as part of our CI setup.
+# The purpose is to install django-ansible-base deps into the tox environment
+# iff requirements have changed.
+
+# Pass in the tox env name as the (only) argument
+ENV_NAME="$1"
+
+DAB_DIR="django-ansible-base"
+REQ_FILE="${DAB_DIR}/requirements/requirements.txt"
+TOX_REQ_FILE=".tox/${ENV_NAME}/django_ansible_base_requirements.txt"
+
+# Exit if $DAB_DIR/.git doesn't exist
+# In this case, tox will just install from git anyway.
+[ ! -d "${DAB_DIR}/.git" ] && exit 0
+
+REQS_CHANGED=0
+if [ -f "$TOX_REQ_FILE" ]; then
+    diff -q "$REQ_FILE" "$TOX_REQ_FILE" > /dev/null
+    REQS_CHANGED=$?
+else
+    REQS_CHANGED=1
+fi
+
+if [ "$REQS_CHANGED" -ne 0 ]; then
+    echo "Changes detected in requirements. Installing django-ansible-base..."
+    pip install "./${DAB_DIR}/"
+    echo "Caching requirements to avoid needlessly reinstalling..."
+    cp "$REQ_FILE" "$TOX_REQ_FILE"
+else
+    echo "No changes detected in requirements. Skipping django-ansible-base install."
+fi
