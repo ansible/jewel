cd717044c55bd7fbd882675e4b5a45af614f24ba
John Westcott IV <john.westcott.iv@redhat.com>
2023-11-28 14:48:06 -0500

    Add SOCIAL_AUTH_USERNAME_IS_FULL_EMAIL setting

---
+++ aap_gateway_api/authentication/util.py
@@ -0,0 +1,10 @@
+import logging
+
+from aap_gateway_api.utils.preferences import get_preference_value
+
+logger = logging.getLogger('aap.gateway.authentication.util')
+
+
+def load_social_auth_settings():
+    logger.info("Loading Gateway social auth settings")
+    return {"SOCIAL_AUTH_USERNAME_IS_FULL_EMAIL": get_preference_value('social_auth', 'SOCIAL_AUTH_USERNAME_IS_FULL_EMAIL', encrypted=False)}
