438b271d35cef6a3004af29c16202a2bea58719f
Red Hat Contributor <arominge@redhat.com>
2024-01-25 15:45:30 -0500

    Copy fixtures from test_app which is not available (#211)

--- aap_gateway_api/tests/conftest.py
+++ aap_gateway_api/tests/conftest.py
@@ -3,18 +3,107 @@ import uuid
 from collections import namedtuple
 
 import pytest
-from test_app.tests.conftest import (  # noqa: F401
-    admin_api_client,
-    copy_fixture,
-    local_authenticator,
-    randname,
-    shut_up_logging,
-    unauthenticated_api_client,
-    user,
-    user_api_client,
-)
 
 from aap_gateway_api.models import AdditionalRoute, ServiceAPIRoute, ServiceCluster, ServiceNode
+
+
+@pytest.fixture
+def shut_up_logging():
+    """
+    This fixture allows you to temporarily disable logging for a test.
+    """
+    import logging
+
+    logging.disable(logging.CRITICAL)
+    yield
+    logging.disable(logging.NOTSET)
+
+
+@pytest.fixture
+def randname():
+    def _randname(prefix):
+        return f"{prefix} {uuid.uuid4().hex[:6]}"
+
+    return _randname
+
+
+def copy_fixture(copies=1):
+    """
+    Decorator to create 'copies' copies of a fixture.
+
+    The copies will be named func_1, func_2, ..., func_n in the same module as
+    the original fixture.
+    """
+
+    def wrapper(func):
+        if '_pytestfixturefunction' not in dir(func):
+            raise TypeError(f"Can't apply copy_fixture to {func.__name__} because it is not a fixture. HINT: @copy_fixture must be *above* @pytest.fixture")
+
+        module_name = func.__module__
+        module = __import__(module_name, fromlist=[''])
+
+        for i in range(copies):
+            new_name = f"{func.__name__}_{i + 1}"
+            setattr(module, new_name, func)
+        return func
+
+    return wrapper
+
+
+@pytest.fixture
+def local_authenticator(db):
+    from ansible_base.authentication.models import Authenticator
+
+    authenticator = Authenticator.objects.create(
+        name="Test Local Authenticator",
+        enabled=True,
+        create_objects=True,
+        users_unique=False,
+        remove_users=True,
+        type="ansible_base.authentication.authenticator_plugins.local",
+        configuration={},
+    )
+    yield authenticator
+    authenticator.authenticator_user.all().delete()
+    authenticator.delete()
+
+
+@pytest.fixture
+def admin_api_client(db, admin_user, unauthenticated_api_client, local_authenticator):
+    client = unauthenticated_api_client
+    client.login(username="admin", password="password")
+    yield client
+    try:
+        client.logout()
+    except AttributeError:
+        # The test might have logged the user out already (e.g. to test the logout signal)
+        pass
+
+
+@pytest.fixture
+def user(db, django_user_model, local_authenticator):
+    user = django_user_model.objects.create_user(username="user", password="password")
+    yield user
+    user.delete()
+
+
+@pytest.fixture
+def user_api_client(db, user, unauthenticated_api_client, local_authenticator):
+    client = unauthenticated_api_client
+    client.login(username="user", password="password")
+    yield client
+    try:
+        client.logout()
+    except AttributeError:
+        # The test might have logged the user out already (e.g. to test the logout signal)
+        pass
+
+
+@pytest.fixture
+def unauthenticated_api_client(db):
+    from rest_framework.test import APIClient
+
+    return APIClient()
 
 
 @pytest.fixture
