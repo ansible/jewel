bde7780268133be5c881b6c20b76120a24e5eaaa
John Westcott IV <32551173+john-westcott-iv@users.noreply.github.com>
2024-01-30 19:06:05 -0500

    [AAP-19927] Remove authenticaiton header if we auth at gateway (#216)
    * Remove authenticaiton header if we auth at gateway
    
    * Allow authorization header to flow back to service if auth fails in gateway

--- aap_gateway_api/proxy/control_plane.py
+++ aap_gateway_api/proxy/control_plane.py
@@ -6,6 +6,7 @@ from django.contrib.sessions.middleware import SessionMiddleware
 from django.http import HttpRequest, parse_cookie
 from envoy.config.core.v3.base_pb2 import HeaderValue, HeaderValueOption
 from envoy.service.auth.v3 import attribute_context_pb2, external_auth_pb2, external_auth_pb2_grpc
+from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.request import Request as DRFRequest
 from rest_framework.settings import api_settings
 
@@ -37,19 +38,22 @@ def get_drf_request(request: attribute_context_pb2.AttributeContext.HttpRequest)
 
 
 class ExternalAuth(external_auth_pb2_grpc.AuthorizationServicer):
-    def _return_authenticated(self, jwt):
+    def _return_authenticated(self, jwt, remove_auth_header):
         logger.info("User successfully authenticated")
 
-        # TODO: DRF basic auth raises an exception if you pass an invalid username/password
-        # regardless of any other auth classes. We need to remove the authentication headers
-        # from the proxied request if auth here succeeds. I'm not going to do this now
-        # because the gateway isn't configured to use JWT token auth yet.
+        # TODO: If we changed gateway to accept its own JWT tokens we could remove this section and just always remove Authorization
+        headers_to_remove = []
+        if remove_auth_header:
+            headers_to_remove.append('Authorization')
 
-        return external_auth_pb2.CheckResponse(
-            ok_response=external_auth_pb2.OkHttpResponse(
-                headers=[HeaderValueOption(header=HeaderValue(key=get_preference_value('proxy', 'gateway_token_name'), value=jwt))]
-            )
+        response = external_auth_pb2.OkHttpResponse(
+            headers=[
+                HeaderValueOption(header=HeaderValue(key=get_preference_value('proxy', 'gateway_token_name'), value=jwt)),
+            ],
+            headers_to_remove=headers_to_remove,
         )
+
+        return external_auth_pb2.CheckResponse(ok_response=response)
 
     def _return_not_authenticated(self):
         logger.info("No authentication")
@@ -64,20 +68,30 @@ class ExternalAuth(external_auth_pb2_grpc.AuthorizationServicer):
         logger.info("Starting authentication.")
         try:
             drf_request = get_drf_request(request.attributes.request.http)
-            user = drf_request.user
+            try:
+                user = drf_request.user
+            except AuthenticationFailed:
+                # Rest framework will raise this exception if the user/pass combo is invalid.
+                # If this is the case we want to fall though and _return_not_authenticated so that the Authorization header will be sent to the backend.
+                user = None
 
-            if not user.pk:
+            if not user or not user.pk:
                 logger.info("No valid credentials found for user.")
                 return self._return_not_authenticated()
 
+            # Gateway will not accept JWT tokens from itself so we have to allow the auth header to remain if we are going to talk with a gateway endpoint
+            remove_auth_header = True
+            if request.attributes.request.http.path.startswith('/api/gateway/'):
+                remove_auth_header = False
+
             if jwt := JWTSessionCache.get(user.pk):
                 logger.info("Loading cached JWT token")
-                return self._return_authenticated(jwt)
+                return self._return_authenticated(jwt, remove_auth_header)
             else:
                 logger.info("Issuing new JWT token")
                 jwt = create_signed_jwt(user)
                 JWTSessionCache.set(user.pk, jwt)
-                return self._return_authenticated(jwt)
+                return self._return_authenticated(jwt, remove_auth_header)
 
         # The GRPC server doesn't seem to be able to catch runtime errors and log a stack trace.
         except Exception as e:
