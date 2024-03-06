52157fbfa8129bc030c1f106a6273cf2f76c3fc3
Chris Meyers <chris.meyers.fsu@gmail.com>
2024-03-06 14:52:14 -0500

    EDA defaults changed to include JWT

--- aap-dev/eda_server_settings.py
+++ aap-dev/eda_server_settings.py
@@ -2,6 +2,5 @@
 # By importing development we respect the default eda-server dev django settings entrypoint
 from .development import *
 
-REST_FRAMEWORK['DEFAULT_AUTHENTICATION_CLASSES'] = ['ansible_base.jwt_consumer.eda.auth.EDAJWTAuthentication',] + REST_FRAMEWORK['DEFAULT_AUTHENTICATION_CLASSES']
 ANSIBLE_BASE_JWT_KEY = 'https://aap-gw-proxy-1:9080/'
 ANSIBLE_BASE_JWT_VALIDATE_CERT = False
