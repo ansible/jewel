4b8f43ad7d5d982a6a06d869b73098bd4233ec32
Red Hat Contributor <kdelee@redhat.com>
2024-03-08 10:00:19 -0500

    read in settings overrides in /etc/gateway/settings.py (#266)
    We already mount in other config files.
    This allows us to optionally mount in django settings.

---
+++ tools/configs/settings.py
@@ -0,0 +1,14 @@
+# Gateway override settings. Settings here will override the default django settings
+
+# GRPC_SERVER_PROCESSES = 5
+# GRPC_SERVER_MAX_THREADS_PER_PROCESS = 10
+# GRPC_SERVER_AUTH_SERVICE_TIMEOUT = '30s'
+
+###############################################################################
+# !!!!!!!!!!!! CAUTION !!!!!!!!!!!
+#
+# This file may be managed by an installer or operator, which
+# will override any changes made to its contents.
+# In order for changes to apply, the gateway service must be restarted
+#
+###############################################################################
