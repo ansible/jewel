3a80faa2955c00bac9e291ba40ad60d02e7199aa
Red Hat Contributor <ricarril@redhat.com>
2024-03-21 17:09:50 +0100

    Add REDIS_ENABLE_TLS flag (#298)

--- aap_gateway_api/settings.py
+++ aap_gateway_api/settings.py
@@ -256,10 +256,11 @@ for env_var in ['REDIS_USER', 'REDIS_PASS', 'REDIS_HOST', 'REDIS_PORT']:
         print(f"Warning, missing {env_var}")
 
 if using_cache:
-    # The double s in rediss is for a TLS secure connection
-    redis_url = f"rediss://{os.environ.get('REDIS_USER')}:{os.environ.get('REDIS_PASS')}@{os.environ.get('REDIS_HOST')}:{os.environ.get('REDIS_PORT')}"
+    protocol = "rediss" if os.environ.get('REDIS_ENABLE_TLS', 'true').lower() in ('true', '1') else "redis"
+    redis_url = f"{protocol}://{os.environ.get('REDIS_USER')}:{os.environ.get('REDIS_PASS')}@{os.environ.get('REDIS_HOST')}:{os.environ.get('REDIS_PORT')}"
     if os.environ.get('REDIS_IGNORE_CERTS', False):
-        redis_url = f"{redis_url}?ssl_cert_reqs=none"
+        redis_url += "?ssl_cert_reqs=none"
+
     CACHES = {
         "default": {
             "BACKEND": "django.core.cache.backends.redis.RedisCache",
