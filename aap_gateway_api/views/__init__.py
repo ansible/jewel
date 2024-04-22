a900c5df750522a44bfe57a9655baf54d3424e9e
John Westcott IV <john.westcott.iv@redhat.com>
2024-04-22 18:22:01 -0400

    Add /session endpoint

--- aap_gateway_api/views/__init__.py
+++ aap_gateway_api/views/__init__.py
@@ -29,6 +29,7 @@ from aap_gateway_api.views.api.v1.service import (  # noqa: F401
     ServiceClusterViewSet,
     ServiceNodeViewSet,
 )
+from aap_gateway_api.views.api.v1.session import SessionView  # noqa: F401
 from aap_gateway_api.views.api.v1.status import StatusView  # noqa: F401
 from aap_gateway_api.views.api.v1.team import TeamViewSet  # noqa: F401
 from aap_gateway_api.views.api.v1.user import UserViewSet  # noqa: F401
