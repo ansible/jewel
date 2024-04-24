16e6520ed5c27ad24aadd284b903db3fdb98234c
John Westcott IV <john.westcott.iv@redhat.com>
2024-04-24 14:00:12 -0400

    Add timing to logs around GRPC events and x-request-id

--- aap_gateway_api/proxy/control_plane.py
+++ aap_gateway_api/proxy/control_plane.py
@@ -1,4 +1,6 @@
 import logging
+import time
+import uuid
 
 from ansible_base.authentication.middleware import AuthenticatorBackendMiddleware
 from django.contrib.auth.middleware import AuthenticationMiddleware
@@ -44,14 +46,21 @@ def get_drf_request(request: attribute_context_pb2.AttributeContext.HttpRequest)
 
 
 class ExternalAuth(external_auth_pb2_grpc.AuthorizationServicer):
-    def _return_authenticated(self, jwt, path, username):
-        logger.info(f"User {username} successfully authenticated for {path}")
+    def _get_ms_delta(self, start_time):
+        delta_ms = (time.time() - start_time) * 1000
+        return f'{delta_ms:.0f} (ms)'
+
+    def _log_process_time(self):
+        logger.debug(f'GRPC process time for {self.request_id}: {self._get_ms_delta(self.start_time)}')
+
+    def _return_authenticated(self, jwt, username):
+        logger.info(f"User {username} successfully authenticated for {self.request_id}")
 
         # Gateway will not accept JWT tokens from itself so we have to allow the auth header to remain if we are going to talk with a gateway endpoint
         # TODO: If we changed gateway to accept its own JWT tokens we could remove this section and just always remove Authorization
         headers_to_remove = []
-        if not path.startswith('/api/gateway/'):
-            logger.debug(f"Removing authorization header for {path}")
+        if not self.request_path.startswith('/api/gateway/'):
+            logger.debug(f"Removing authorization header for {self.request_id}")
             headers_to_remove.append('Authorization')
 
         response = external_auth_pb2.OkHttpResponse(
@@ -61,28 +70,43 @@ class ExternalAuth(external_auth_pb2_grpc.AuthorizationServicer):
             headers_to_remove=headers_to_remove,
         )
 
+        self._log_process_time()
         return external_auth_pb2.CheckResponse(ok_response=response)
 
-    def _return_no_authentication_required(self, path):
+    def _return_no_authentication_required(self):
         # This endpoint did not require authentication so no logs required
-        logger.debug(f"No authentication required for {path}")
+        logger.debug(f"No authentication required for {self.request_id}")
+        self._log_process_time()
         return external_auth_pb2.CheckResponse(ok_response=external_auth_pb2.OkHttpResponse())
 
-    def _return_not_authenticated(self, path):
-        logger.info(f"No valid credentials found for user when requesting {path}.")
+    def _return_not_authenticated(self):
+        logger.info(f"No valid credentials found for user when requesting {self.request_id}.")
 
         # We're returning an OK response instead of a 403 because the user may have
         # a local credential for the service, or may be requesting an API endpoint
         # that doesn't require authentication. The final decision on whether or
         # not to accept the request is up to the service.
+        self._log_process_time()
         return external_auth_pb2.CheckResponse(ok_response=external_auth_pb2.OkHttpResponse())
 
     def Check(self, request, context):
-        request_path = request.attributes.request.http.path
-        if request_path in no_authentication_required or request_path.startswith('/static/'):
-            return self._return_no_authentication_required(request_path)
+        self.start_time = time.time()
 
-        logger.info(f"Starting authentication for {request_path}.")
+        self.request_path = request.attributes.request.http.path
+        self.request_id = 'none'
+        user_request_id = request.attributes.request.http.headers.get('x-request-id', None)
+        if user_request_id:
+            try:
+                uuid.UUID(str(user_request_id))
+                self.request_id = str(user_request_id)
+            except ValueError:
+                pass
+
+        logger.info(f"Starting authentication for ({self.request_id}) {self.request_path}.")
+
+        if self.request_path in no_authentication_required or self.request_path.startswith('/static/'):
+            return self._return_no_authentication_required()
+
         try:
             drf_request = get_drf_request(request.attributes.request.http)
             try:
@@ -93,19 +117,21 @@ class ExternalAuth(external_auth_pb2_grpc.AuthorizationServicer):
                 user = None
 
             if not user or not user.pk:
-                return self._return_not_authenticated(request_path)
+                return self._return_not_authenticated()
 
             if jwt := JWTSessionCache.get(user.pk):
-                logger.debug(f"Loading cached JWT token for {user.username}")
+                logger.debug(f"Loading cached JWT token for {user.username} ({self.request_id})")
             else:
-                logger.debug(f"Issuing new JWT token for {user.username}")
+                logger.debug(f"Issuing new JWT token for {user.username} ({self.request_id})")
+                jwt_start = time.time()
                 jwt = create_signed_jwt(user)
+                logger.debug(f"GRPC took {self._get_ms_delta(jwt_start)} to create a signed JWT ({self.request_id})")
                 JWTSessionCache.set(user.pk, jwt)
 
-            return self._return_authenticated(jwt, request_path, user.username)
+            return self._return_authenticated(jwt, user.username)
 
         except (DatabaseError, OperationalError) as e:
-            logger.warning("Database error. We think it's a connection error. Resetting the connection so it can be tried again.")
+            logger.warning("Database error. We think it's a connection error. Resetting the connection so it can be tried again. ({self.request_id})")
             logger.debug(e, exc_info=True)
             for conn in connections.all():
                 conn.close_if_unusable_or_obsolete()
