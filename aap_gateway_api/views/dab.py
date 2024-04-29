51d7afbd8ac7c4f1859094fe041272df5ede6aaf
Martin Slemr <mslemr@redhat.com>
2024-04-29 21:25:53 +0200

    Remove Custom parent view for DAB (#344)
    * Remove Custom parent view for DAB
    
    * Removing DABView class

--- aap_gateway_api/views/dab.py
+++
@@ -1,7 +0,0 @@
-from ansible_base.lib.utils.views.ansible_base import AnsibleBaseView
-
-from aap_gateway_api.permissions import IsSystemAdminOrAuditor
-
-
-class GatewayDABBase(AnsibleBaseView):
-    permission_classes = [IsSystemAdminOrAuditor]
