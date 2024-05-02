7647b55b69358a8a7c5e013754bea6421168b534
John Westcott IV <john.westcott.iv@redhat.com>
2024-05-02 05:16:02 -0400

    Add file logger

--- tools/configs/settings.py
+++ tools/configs/settings.py
@@ -1,5 +1,9 @@
 # gateway override settings. Settings here will override the default django settings
 
+# Turn on file logging
+# LOGGING['handlers']['file']['class'] = 'logging.handlers.WatchedFileHandler'
+# LOGGING['handlers']['file']['filename'] = '/var/log/ansible-automation-platform/gateway/gateway.log'
+
 # GRPC_SERVER_PROCESSES = 5
 # GRPC_SERVER_MAX_THREADS_PER_PROCESS = 10
 # GRPC_SERVER_AUTH_SERVICE_TIMEOUT = '30s'
