026573aaccd5244d5d6f771185b0d3e2ec3f0b87
Red Hat Contributor <lyasin@redhat.com>
2024-05-23 16:20:30 -0400

    Prevent deletion and edit of the name of the default gateway organization in gateway API (#384)
    * Prevent deletion of managed organizations from the org view, with test
    ---------
    Signed-off-by: Lila <noreply@redhat.com>
    Co-authored-by: John Westcott IV <john.westcott.iv@redhat.com>

--- aap_gateway_api/views/api/v1/organization.py
+++ aap_gateway_api/views/api/v1/organization.py
@@ -1,6 +1,14 @@
+import logging
+
+from django.utils.translation import gettext_lazy as _
+from rest_framework import status
+from rest_framework.response import Response
+
 from aap_gateway_api.models import Organization
 from aap_gateway_api.serializers import OrganizationSerializer
 from aap_gateway_api.views.api.v1.common import ResourceAPIUpdateMixin, RoleModelViewSet
+
+logger = logging.getLogger('aap.gateway.views.organization')
 
 
 class OrganizationViewSet(ResourceAPIUpdateMixin, RoleModelViewSet):
@@ -10,3 +18,12 @@ class OrganizationViewSet(ResourceAPIUpdateMixin, RoleModelViewSet):
 
     queryset = Organization.objects.select_related("resource").all()
     serializer_class = OrganizationSerializer
+
+    # Don't allow the deletion of any managed organizations
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance.managed:
+            logger.info("Managed organizations cannot be deleted.")
+            return Response(status=status.HTTP_400_BAD_REQUEST, data={"details": _("Managed organizations cannot be deleted.")})
+        else:
+            return super().destroy(request, *args, **kwargs)
