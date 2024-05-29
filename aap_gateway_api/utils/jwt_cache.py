8c156f91876589a5c88cde6cd7b37fc214ff078f
jctanner <tanner.jc@gmail.com>
2024-05-29 17:45:44 -0400

    Invalidate the token cache -before- the token expires. (#390)
    * Invalidate the token cache -before- the token expires.
    * Add a setting for the jwt cache invalidation delta.
    * Change the preference name.
    * Add unit test.
    * Use less ambiguous test values.
    
    No-Issue
    
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- aap_gateway_api/utils/jwt_cache.py
+++ aap_gateway_api/utils/jwt_cache.py
@@ -8,7 +8,13 @@ class JWTSessionCache:
     def set(user_pk, jwt):
         from aap_gateway_api.utils import get_preference_value
 
-        return cache.set(JWT_SESSION_PREFIX + str(user_pk), jwt, timeout=get_preference_value("proxy", "gateway_access_token_expiration"))
+        # try to invalidate the cache -before- the token expires so that
+        # there isn't a window of time where the cache serves out an expired
+        # token and causes 401s for the client.
+        delta = get_preference_value("proxy", "jwt_expiration_buffer_in_seconds")
+        timeout = get_preference_value("proxy", "gateway_access_token_expiration") - delta
+
+        return cache.set(JWT_SESSION_PREFIX + str(user_pk), jwt, timeout=timeout)
 
     @staticmethod
     def get(user_pk):
