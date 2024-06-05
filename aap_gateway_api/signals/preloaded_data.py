db49e592c11ff18187a34ebf0da5ba8444aef862
John Westcott IV <32551173+john-westcott-iv@users.noreply.github.com>
2024-06-05 13:26:56 -0400

    Create preloaded data (#399)
    * Create preloaded data
    
    * Alter existing tests to compensate for the Default organization

---
+++ aap_gateway_api/signals/preloaded_data.py
@@ -0,0 +1,54 @@
+import logging
+
+from django.apps import apps as global_apps
+
+logger = logging.getLogger('aap.gateway.signals.preloaded_data')
+
+
+def create_preload_data(**kwargs) -> None:
+    """
+    Run any function in this file whose name starts with 'create_'
+    All functions in this code should take no arguments and be idempotent
+    If they fail, an exception (of any type) should be raised
+    If an exception is raised the message "Failed to <function name replace _ with ' '>" is outputted in the logs
+    """
+
+    # Verbosity comes from the signal see https://docs.djangoproject.com/en/5.0/ref/signals/#post-migrate
+    verbosity = kwargs.get('verbosity', 1)
+
+    # Plan comes from the signal as well.
+    # If this got called outside of a signal or presumably from a flush it may not exist
+    if 'plan' not in kwargs:
+        # If we are are being called via a flush instead of a migrate then we can just return
+        return
+
+    for migration, rolled_back in kwargs['plan']:
+        if rolled_back:
+            if verbosity > 0:
+                logger.debug(f"We are rolling back migration {migration}, no need to create objects")
+            return
+
+    if verbosity > 0:
+        logger.info("Building preloaded data")
+
+    for function in [create_default_organization]:
+        name = function.__name__
+        try:
+            if verbosity > 1:
+                logger.info(f"Running {name}")
+            created = function()
+            if verbosity > 0 and created:
+                logger.debug(f"Created {' '.join(name.split('_')[1:])}")
+        except Exception as e:
+            if verbosity in [0, 1]:
+                logger.error(f"Failed to {name.replace('_', ' ')} {e}")
+            elif verbosity > 1:
+                logger.exception(f"Failed to {name.replace('_', ' ')}")
+
+
+def create_default_organization() -> bool:
+    Organization = global_apps.get_model('aap_gateway_api', 'Organization')
+    _org, created = Organization.objects.get_or_create(
+        name='Default', defaults={'managed': True, 'description': 'The default organization for Ansible Automation Platform'}
+    )
+    return created
---
+++ aap_gateway_api/tests/signals/test_preloaded_data.py
@@ -0,0 +1,54 @@
+from unittest import mock
+
+import pytest
+
+from aap_gateway_api.models import Organization
+from aap_gateway_api.signals.preloaded_data import create_default_organization, create_preload_data
+
+
+class TestCreatePreloadedData:
+    @pytest.mark.django_db
+    def test_create_default_organization(self):
+        Organization.objects.all().delete()
+        assert create_default_organization() is True, "We should have created the default organization"
+        assert create_default_organization() is False, "We should not have recreated the default organization"
+
+    @pytest.mark.django_db
+    def test_default_org_created_by_default(self):
+        org = Organization.objects.filter(name='Default')
+        assert org
+
+    @pytest.mark.django_db
+    def test_immediate_return_if_no_plan(self):
+        Organization.objects.all().delete()
+        create_preload_data()
+        assert Organization.objects.count() == 0
+
+    @pytest.mark.django_db
+    @pytest.mark.parametrize(
+        "verbosity",
+        [
+            0,
+            1,
+        ],
+    )
+    def test_no_run_on_rollback(self, verbosity, expected_log):
+        Organization.objects.all().delete()
+        with expected_log('aap_gateway_api.signals.preloaded_data.logger', 'debug', 'We are rolling back migration', assert_not_called=(not verbosity)):
+            create_preload_data(verbosity=verbosity, plan=[('0000', False), ('0001', True)])
+        assert Organization.objects.count() == 0
+
+    @pytest.mark.parametrize(
+        "verbosity,log_level",
+        [
+            (0, 'error'),
+            (2, 'exception'),
+        ],
+    )
+    def test_exception_raised_during_create(self, verbosity, log_level, expected_log):
+        mock_function = mock.MagicMock()
+        mock_function.__name__ = 'create_default_organization'
+        mock_function.side_effect = Exception('Failed')
+        with mock.patch('aap_gateway_api.signals.preloaded_data.create_default_organization', mock_function):
+            with expected_log('aap_gateway_api.signals.preloaded_data.logger', log_level, 'Failed to'):
+                create_preload_data(verbosity=verbosity, plan=[('0000', False)])
