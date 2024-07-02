029ecc27ba4f0df1a2a2d4f79ce4cc6371bb4cc7
Alan Rominger <arominge@redhat.com>
2024-07-02 12:53:51 -0400

    Add a TSD file for the aap-dev environment (#446)

--- aap-dev/controller_settings.py
+++ aap-dev/controller_settings.py
@@ -2,5 +2,5 @@ OPTIONAL_API_URLPATTERN_PREFIX = 'controller'
 ANSIBLE_BASE_JWT_KEY = 'https://aap-gw-proxy-1:9080/'
 ANSIBLE_BASE_JWT_VALIDATE_CERT = False
 ANSIBLE_BASE_JWT_REDIRECT_TYPE= "awx"
-ALLOW_LOCAL_RESOURCE_MANAGEMENT = False
+ALLOW_LOCAL_RESOURCE_MANAGEMENT = True  # Temporary: ATF test-suite needs this setting
 ANSIBLE_BASE_MANAGED_ROLE_REGISTRY = {'platform_auditor': {'name': 'Platform Auditor', 'shortname': 'sys_auditor'}}
--- aap-dev/eda_server_settings.py
+++ aap-dev/eda_server_settings.py
@@ -4,5 +4,5 @@ from .development import *
 
 ANSIBLE_BASE_JWT_KEY = 'https://aap-gw-proxy-1:9080/'
 ANSIBLE_BASE_JWT_VALIDATE_CERT = False
-ALLOW_LOCAL_RESOURCE_MANAGEMENT = False
+ALLOW_LOCAL_RESOURCE_MANAGEMENT = True  # Temporary: ATF test-suite needs this setting
 ANSIBLE_BASE_MANAGED_ROLE_REGISTRY = {'platform_auditor': {'name': 'Platform Auditor', 'shortname': 'sys_auditor'}}
