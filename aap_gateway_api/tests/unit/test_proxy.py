e12751ada53de9eb56932aedff3e52cef4079443
Brennan Paciorek <50780403+BrennanPaciorek@users.noreply.github.com>
2024-07-24 09:17:15 -0400

    Enforce CSRF verification on envoy external authentication (#452)
    The proxy disabled csrf mitigations when doing authentication checks, then appended
    JWT headers when sending to service, which introduces a CSRF vulnerability when
    a user authenticates to envoy using cookie-based authentication mechanisms.
    
    This change fixes this by running full CSRF checks as needed at the proxy level.
    
    Fixes: AAP-20597

---
+++ aap_gateway_api/tests/unit/test_proxy.py
@@ -0,0 +1,130 @@
+from json import dumps
+from os import linesep
+from unittest import mock
+
+import pytest
+from rest_framework.authentication import SessionAuthentication
+
+from aap_gateway_api.proxy.control_plane import ExternalAuth, get_drf_request
+
+request_body_multipart = '''-----------------------------25667258076756890893396248524
+Content-Disposition: form-data; name=\"csrfmiddlewaretoken\"
+
+NtKB9gTfzeqBQD7l0It7BlH5nN6SUBQPNTkXrE8XTGFj2n13C1qTSmdhIwxGZSeV
+-----------------------------25667258076756890893396248524
+'''.replace(
+    linesep, "\r\n"
+)
+
+request_body_json = dumps(
+    {
+        "csrfmiddlewaretoken": "NtKB9gTfzeqBQD7l0It7BlH5nN6SUBQPNTkXrE8XTGFj2n13C1qTSmdhIwxGZSeV",
+    }
+)
+
+request_headers = {
+    'SEC_FETCH_SITE': 'same-origin',
+    'X_FORWARDED_FOR': '172.21.0.1',
+    'X_ENVOY_INTERNAL': 'true',
+    'REFERER': 'https://localhost/api/galaxy/v3/namespaces/',
+    'DNT': '1',
+    'X_REQUEST_ID': '9db8fa3e-ea44-4c53-9b0a-52b3f18fbcd5',
+    'ACCEPT_ENCODING': 'gzip, deflate, br, zstd',
+    'SEC_FETCH_MODE': 'navigate',
+    ':AUTHORITY': 'localhost',
+    'PRAGMA': 'no-cache',
+    'UPGRADE_INSECURE_REQUESTS': '1',
+    'CONTENT_LENGTH': '1147',
+    ':PATH': '/api/galaxy/v3/namespaces/',
+    'COOKIE': 'csrftoken=aAKwsypSuCpSmU4SMt7WrbGmvTBYfryg; tabstyle=html-tab',
+    'X_ENVOY_AUTH_PARTIAL_BODY': 'false',
+    'ACCEPT_LANGUAGE': 'en-US,en;q=0.5',
+    ':SCHEME': 'https',
+    'USER_AGENT': 'Mozilla/5.0 (X11; Linux x86_64; rv:127.0) Gecko/20100101 Firefox/127.0',
+    'CONTENT_TYPE': 'multipart/form-data; boundary=---------------------------25667258076756890893396248524',
+    'ACCEPT': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+    'SEC_FETCH_USER': '?1',
+    ':METHOD': 'POST',
+    'SEC_FETCH_DEST': 'document',
+    'X_FORWARDED_PROTO': 'https',
+    'CACHE_CONTROL': 'no-cache',
+    'PRIORITY': 'u=1',
+}
+
+
+class Request:
+    def __init__(self, method="GET", host="localhost", path="/", header_diff={}, body="", query=""):
+        self.method = method
+        self.host = host
+        self.path = path
+        self.raw_body = bytes(body, "utf-8")
+        self.headers = request_headers.copy()
+        self.headers.update(header_diff)
+        self.query = query
+        self.headers["CONTENT_LENGTH"] = str(len(self.raw_body))
+
+        self.attributes = self
+        self.request = self
+        self.http = self
+
+
+@pytest.mark.parametrize(
+    "method,host,path,body,headers",
+    [
+        ("POST", "localhost", "/api/gateway/v3/namespaces", request_body_multipart, {}),
+        (
+            "POST",
+            "localhost",
+            "/api/gateway/v3/namespaces",
+            request_body_json,
+            {
+                "CONTENT_TYPE": "application/json; charset=utf-8",
+            },
+        ),
+    ],
+)
+def test_get_drf_request(method, host, path, body, headers):
+    request = Request(method=method, host=host, path=path, body=body, header_diff=headers)
+    drf_req = get_drf_request(request)
+
+    assert "csrfmiddlewaretoken" in drf_req.data
+
+
+@pytest.fixture
+def ext_auth():
+    yield ExternalAuth()
+
+
+class MockSessionAuth(SessionAuthentication):
+    def authenticate(self, request):
+        # Skip authentication, start enforcing csrf verification
+        self.enforce_csrf(request)
+
+        return "admin", ""
+
+
+@pytest.mark.django_db
+class TestExternalAuth:
+    @pytest.mark.parametrize(
+        "method,host,path,body,headers",
+        [
+            # json requests with no token header will fail csrf verification, since body is not checked in this case
+            (
+                "POST",
+                "localhost",
+                "/api/galaxy/v3/namespaces",
+                request_body_json,
+                {
+                    "CONTENT_TYPE": "application/json; charset=utf-8",
+                },
+            ),
+        ],
+    )
+    def test_check_bad_csrf(self, method, host, path, body, headers, ext_auth):
+        request = Request(method=method, host=host, path=path, body=body, header_diff=headers)
+        response = None
+        with mock.patch("rest_framework.authentication.SessionAuthentication.authenticate", MockSessionAuth.authenticate):
+            response = ext_auth.Check(request, None)
+
+        # assert permission denied
+        assert response.status.code == 7
