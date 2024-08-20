81b164a7716cb7708536eb9c5147cdacb4d0b820
Rick Elrod <rick@elrod.me>
2024-08-20 08:40:15 -0500

    [resource api] Do not error if a resource already exists (#515)
    If multiple services try to create a resource at the exact same time,
    one of them would previously ultimately fail during reverse sync.
    
    This fixes that by making POST operations on the resource api use
    update_or_create instead.
    
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- aap_gateway_api/resource_api.py
+++ aap_gateway_api/resource_api.py
@@ -1,11 +1,40 @@
+import copy
+
 from ansible_base.resource_registry.registry import ParentResource, ResourceConfig, ServiceAPIConfig, SharedResource
 from ansible_base.resource_registry.shared_types import OrganizationType, TeamType, UserType
+from ansible_base.resource_registry.utils.resource_type_processor import ResourceTypeProcessor
 
 from aap_gateway_api import models
+from aap_gateway_api.utils.models import unique_fields_for_model
+
+
+class GetOrCreateProcessor(ResourceTypeProcessor):
+    def save(self, validated_data, is_new=False):
+        # This doesn't _really_ mean "new", it more means that we are POSTing
+        if is_new:
+            unique_fields = unique_fields_for_model(self.instance.__class__)
+
+            # pop out the unique fields - don't modify the input data, though
+            validated_data = copy.deepcopy(validated_data)
+            unique_fields = {k: validated_data.pop(k) for k in unique_fields if k in validated_data}
+
+            self.instance, created = self.instance.__class__.objects.update_or_create(**unique_fields, defaults=validated_data)
+            return self.instance
+
+        for k, val in validated_data.items():
+            setattr(self.instance, k, val)
+
+        self.instance.save()
+        return self.instance
 
 
 class APIConfig(ServiceAPIConfig):
     service_type = "aap"
+    custom_resource_processors = {
+        "shared.organization": GetOrCreateProcessor,
+        "shared.team": GetOrCreateProcessor,
+        "shared.user": GetOrCreateProcessor,
+    }
 
 
 RESOURCE_LIST = (
