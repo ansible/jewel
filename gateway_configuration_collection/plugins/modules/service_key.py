e3e009cad157696c73e2309558d67603c4780e6f
John Westcott IV <john.westcott.iv@redhat.com>
2024-10-08 17:23:22 -0400

    Enable return of secret in service_key on creation

--- gateway_configuration_collection/plugins/modules/service_key.py
+++ gateway_configuration_collection/plugins/modules/service_key.py
@@ -98,7 +98,7 @@ def main():
 
     # Create a module with spec
     module = AAPModule(argument_spec=argument_spec, supports_check_mode=True)
-    AAPServiceKey(module).manage()
+    AAPServiceKey(module).manage(json_output_fields=['secret'])
 
 
 if __name__ == "__main__":
