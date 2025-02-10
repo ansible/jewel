3fff9d5718b8e7e71e56d15726a6d7ba00cf2fec
Zack Kayyali <noreply@redhat.com>
2025-02-10 16:00:20 -0500

    Gateway: add installer feature-flag toggle (#709)
    * gateway: add installer toggle
    
    * add unit tests
    
    * enhance unit test
    
    * remove function, set from locals directly
    
    * rename function, enhance defaulting

--- aap_gateway_api/tests/feature_flags/test_feature_flag_api.py
+++ aap_gateway_api/tests/feature_flags/test_feature_flag_api.py
@@ -1,7 +1,11 @@
 import pytest
 from ansible_base.lib.utils.response import get_relative_url
+from django.conf import settings
 from django.test import override_settings
+from flags.state import flag_state
 from rest_framework import status
+
+from aap_gateway_api.settings import _set_feature_flags
 
 
 def test_feature_flags_list_endpoint(admin_api_client):
@@ -32,3 +36,20 @@ def test_feature_flags_override_flags(admin_api_client):
     assert len(response.data) == 2
     assert response.data["FEATURE_SOME_PLATFORM_FLAG_ENABLED"] is False
     assert response.data["FEATURE_SOME_PLATFORM_FLAG_FOO_ENABLED"] is True
+
+
+@override_settings(
+    FLAGS={
+        "FEATURE_SOME_PLATFORM_FLAG_ENABLED": [
+            {"condition": "boolean", "value": False},
+        ],
+    },
+)
+@pytest.mark.django_db
+def test_set_feature_flags(admin_api_client):
+    _set_feature_flags(settings.FLAGS, {"FEATURE_SOME_PLATFORM_FLAG_ENABLED": True})
+    assert flag_state("FEATURE_SOME_PLATFORM_FLAG_ENABLED") is True
+    url = get_relative_url("featureflags-list")
+    response = admin_api_client.get(url)
+    assert response.status_code == status.HTTP_200_OK, response.data
+    assert response.data["FEATURE_SOME_PLATFORM_FLAG_ENABLED"] is True
