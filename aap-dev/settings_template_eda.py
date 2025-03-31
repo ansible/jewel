0c9c296ab784e32440eb09735f2864792a39d113
Zack Kayyali <zkayyali812@gmail.com>
2025-03-31 10:51:05 -0400

    Fix EDA Dev Environment (#743)
    Fixes AAP EDA Dev env

--- aap-dev/settings_template_eda.py
+++ aap-dev/settings_template_eda.py
@@ -1,9 +1,1 @@
-# This file will be the django settings entry point
-# By importing development we respect the default eda-server dev django settings entrypoint
-from .development import *
-
-# This defaults to False but is turned True for normal dev environment
-# since we are hooking it up to Gateway, we turn it back to False
-ALLOW_SHARED_RESOURCE_CUSTOM_ROLES = False
-
-# Will append the common settings
+ALLOW_SHARED_RESOURCE_CUSTOM_ROLES=True
