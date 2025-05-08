da246621419535e001d6ac86cd901729ae4925da
Red Hat Contributor <tduong@redhat.com>
2025-05-08 02:23:07 -0400

    Refactor handle method to reduce its cognitive complexity

--- aap_gateway_api/management/commands/list_services.py
+++ aap_gateway_api/management/commands/list_services.py
@@ -5,34 +5,46 @@ from aap_gateway_api.models import AdditionalRoute, ServiceAPIRoute, ServiceClus
 
 class Command(BaseCommand):
     help = "List all clusters, nodes and routes."
+    route_props = [
+        'http_port',
+        'gateway_path',
+        'enable_gateway_auth',
+        'is_service_https',
+        'service_port',
+        'service_path',
+        'serviceapiroute',
+        'api_slug',
+    ]
 
     def handle(self, *args, **options):
-        # display each of the these route properties
-        route_props = [
-            'http_port',
-            'gateway_path',
-            'enable_gateway_auth',
-            'is_service_https',
-            'service_port',
-            'service_path',
-            'serviceapiroute',
-            'api_slug',
-        ]
+        """
+        print all nodes for a given cluster and
+        display each of the route property in self.route_props
+        """
 
         # iterate through service clusters
         for cluster in ServiceCluster.objects.all():
             self.stdout.write(f'\ncluster: {cluster}')
+            self._print_nodes_in_cluster(cluster)
+            self._print_routes_in_cluster(cluster)
 
-            # print out all the nodes in the cluster
-            for node in ServiceNode.objects.filter(service_cluster=cluster):
-                self.stdout.write(f'\tnode: {node}')
+    def _print_nodes_in_cluster(self, cluster):
+        """
+        print all nodes for a given cluster.
+        """
+        for node in ServiceNode.objects.filter(service_cluster=cluster):
+            self.stdout.write(f"\tnode: {node}")
 
-            # iterate through route types
-            for qs in [ServiceAPIRoute, AdditionalRoute]:
-                for route in qs.objects.filter(service_cluster=cluster):
-                    self.stdout.write(f'\t{qs.__name__}: {route.name}')
-                    for rprop in route_props:
-                        # "additional routes" don't have all the same props
-                        if hasattr(route, rprop):
-                            val = getattr(route, rprop)
-                            self.stdout.write(f'\t\t{rprop}: {val}')
+    def _print_routes_in_cluster(self, cluster):
+        """
+        print all API and additional routes and their properties.
+        """
+        # iterate through route types
+        for qs in [ServiceAPIRoute, AdditionalRoute]:
+            for route in qs.objects.filter(service_cluster=cluster):
+                self.stdout.write(f'\t{qs.__name__}: {route.name}')
+                for rprop in self.route_props:
+                    # "additional routes" don't have all the same props
+                    if hasattr(route, rprop):
+                        val = getattr(route, rprop)
+                        self.stdout.write(f'\t\t{rprop}: {val}')
