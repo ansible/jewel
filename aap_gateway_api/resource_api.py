a4bcc834044647108fad3ca333e96b172a1089b8
Truc Duong <91285203+trucdg@users.noreply.github.com>
2025-05-23 00:36:47 -0400

    AAP-45809 Reduce unique_fields_for_model() cognitive complexity (#786)
    * refactor code that handles model instances lookup in resource GetOrCreateProcessor

--- aap_gateway_api/resource_api.py
+++ aap_gateway_api/resource_api.py
@@ -5,20 +5,31 @@ from ansible_base.resource_registry.shared_types import OrganizationType, TeamTy
 from ansible_base.resource_registry.utils.resource_type_processor import ResourceTypeProcessor
 
 from aap_gateway_api import models
-from aap_gateway_api.utils.models import unique_fields_for_model
+from aap_gateway_api.utils.models import get_model_lookup_keys
 
 
 class GetOrCreateProcessor(ResourceTypeProcessor):
     def save(self, validated_data, is_new=False):
-        # This doesn't _really_ mean "new", it more means that we are POSTing
+        """
+        Save the resource instance using the provided validated_data.
+        If `is_new` is True (i.e: for POST requests), this method tries to find an existing object using the model's unique fields.
+        If found, it updates the existing objects. If not, it creates a new one.
+        If `is_new` is False (i.e: for PUT/ PATCH requests), this method sets the fields in validated_data accordingly.
+        """
+
         if is_new:
-            unique_fields = unique_fields_for_model(self.instance.__class__)
+            # find the fields of this model that can be used to find an existing instance
+            lookup_fields = get_model_lookup_keys(self.instance.__class__)
 
-            # pop out the unique fields - don't modify the input data, though
+            # get the look up fields kwargs that are present in the validated_data without modifying the input data
             validated_data = copy.deepcopy(validated_data)
-            unique_fields = {k: validated_data.pop(k) for k in unique_fields if k in validated_data}
+            lookup_kwargs = {k: validated_data.pop(k) for k in lookup_fields if k in validated_data}
 
-            self.instance, created = self.instance.__class__.objects.update_or_create(**unique_fields, defaults=validated_data)
+            # we use update_or_create to ensure idempotency for repeated POSTS
+            # this is to support cases where multiple services might make simultaneous requests to create a shared resource.
+            # If a resource is being created locally in a service and the resource already exists on Gateway, the local resource
+            # should be linked to the resource in Gateway
+            self.instance, _ = self.instance.__class__.objects.update_or_create(**lookup_kwargs, defaults=validated_data)
             return self.instance
 
         for k, val in validated_data.items():
