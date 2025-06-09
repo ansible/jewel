45a07470f997eb508d45fd16201e3633a7b8e54d
Truc Duong <91285203+trucdg@users.noreply.github.com>
2025-06-09 10:27:43 -0400

    [AAP-46508) refactor V1RootView.get() and improve reverse lookup logic  (#796)
    * refactor V1RootView.get
    
    * improve reverse lookup logic and update ignored_endpoints to prevent unnecessary view logger errors

--- aap_gateway_api/views/api/v1/__init__.py
+++ aap_gateway_api/views/api/v1/__init__.py
@@ -16,7 +16,10 @@ from rest_framework.schemas.generators import EndpointEnumerator
 logger = logging.getLogger('aap.gateway.views')
 
 
-ignore_endpoints = ['docs']
+ignore_endpoints = ['docs', 'service-index', 'jwt_key', 'role_metadata', 'feature_flags_state']
+
+
+GW_V1_ROOT_URL = "/api/gateway/v1"
 
 
 @extend_schema(request=None, responses={"200": OpenApiTypes.OBJECT})
@@ -25,19 +28,14 @@ class V1RootView(AnsibleBaseView):
     name = _('v1')
     versioning_class = None
 
-    @method_decorator(ensure_csrf_cookie)
-    def get(self, request, format=None):
+    def _find_endpoints_reverse_lookup_names(self, endpoints):
+        """
+        Attempts to retrieve all possible endpoint names for reverse URL lookup.
+        If no matching endpoint name is found, logs an error message.
+        Returns:
+        - data(dict): A mapping of endpoint names to their corresponding URL paths
+        """
         data = OrderedDict()
-        enumerator = EndpointEnumerator()
-        endpoints = []
-        for endpoint, junk, junk in enumerator.get_api_endpoints():
-            if not endpoint.startswith('/api/gateway/v1'):
-                continue
-
-            endpoint = endpoint.replace('/api/gateway/v1/', '').split('/')[0]
-            if endpoint and endpoint not in endpoints:
-                endpoints.append(endpoint)
-
         endpoints.sort()
         for endpoint in endpoints:
             if endpoint in ignore_endpoints:
@@ -47,7 +45,16 @@ class V1RootView(AnsibleBaseView):
             if endpoint == 'status':
                 singular_endpoint = endpoint
 
-            for possible_view_name in [f'{singular_endpoint}-list', f'{singular_endpoint}-view', f"{singular_endpoint.replace('_', '')}-list"]:
+            # Now, we try to form all possible view name patterns
+            no_underscore = singular_endpoint.replace('_', '')
+            kebab_case = singular_endpoint.replace('_', '-')
+
+            suffixes = ["-list", "-view"]
+
+            # for each base, create a possible view name
+            possible_view_name_list = [f"{b}{suffix}" for b in [singular_endpoint, no_underscore, kebab_case] for suffix in suffixes]
+
+            for possible_view_name in possible_view_name_list:
                 try:
                     data[endpoint] = get_relative_url(possible_view_name)
                 except NoReverseMatch:
@@ -55,5 +62,26 @@ class V1RootView(AnsibleBaseView):
 
             if endpoint not in data:
                 logger.error(f'{singular_endpoint} had neither a -list nor -view reverse lookup method, ignoring')
+
+        return data
+
+    @method_decorator(ensure_csrf_cookie)
+    def get(self, request, format=None):
+        """
+        This responds to GET requests made to it with a sorted list of available API endpoints
+        """
+        enumerator = EndpointEnumerator()
+        endpoints = []
+
+        # we're using 'junk' because '_' is already used as an alias for gettext_lazy
+        for endpoint, junk, junk in enumerator.get_api_endpoints():
+            if not endpoint.startswith(GW_V1_ROOT_URL):
+                continue
+
+            endpoint = endpoint.replace(GW_V1_ROOT_URL + "/", '').split('/')[0]
+            if endpoint and endpoint not in endpoints:
+                endpoints.append(endpoint)
+
+        data = self._find_endpoints_reverse_lookup_names(endpoints)
 
         return Response(data)
