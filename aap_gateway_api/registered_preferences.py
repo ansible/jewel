f280d7f3c822474b52b49125f237160df7a9a3a1
Red Hat Contributor <noreply@redhat.com>
2025-06-30 12:19:00 -0400

    change: resource_client_request_timeout from 2.0 to 10.0 (#848)

--- aap_gateway_api/registered_preferences.py
+++ aap_gateway_api/registered_preferences.py
@@ -108,7 +108,7 @@ register(
 register(
     section="proxy",
     preference_name="resource_client_request_timeout",
-    default=2.0,
+    default=10.0,
     required=True,
     preference_type="float_range",
     help_text=_("The timeout (in seconds) before the resource client will drop requests after forming connections."),
