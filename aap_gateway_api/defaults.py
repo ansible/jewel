4cd5ae91df4a14c52ae6e1462f6c22c8e75dbc36
Tomas Z <tznamena@redhat.com>
2026-06-04 10:52:04 -0400

    Change the settings to use DABRedisCache from DAB.
    Signed-off-by: Tomas Z <tznamena@redhat.com>

--- aap_gateway_api/defaults.py
+++ aap_gateway_api/defaults.py
@@ -47,6 +47,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 CACHES = {
     "default": {
+        "BACKEND": "ansible_base.lib.cache.redis_cache.DABRedisCache",
+        "LOCATION": "unix:///var/run/redis/redis.sock?db=4",
+        "KEY_PREFIX": "gateway",
+    },
+    "legacy": {
         "BACKEND": "ansible_base.lib.cache.fallback_cache.DABCacheWithFallback",
     },
     "primary": {
